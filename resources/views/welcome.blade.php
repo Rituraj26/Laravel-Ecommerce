075ae3322801c303c4ea750c7f8dc8e396dfd35a
Rituraj <riturajd265@gmail.com>
2021-03-19 12:09:53 +0000

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="./css/app.css">
+
+</head>
+
+<body class="antialiased">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="ms-5 text-primary">hello</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, nobis.</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
